2e4e380e57fe1c6e0fc23e2de2d708fddd00ea10
ahmed oueslati <oueslatiahmed38@gmail.com>
2025-07-05 19:23:23 +0200

    msg

--- Rps.js
+++ Rps.js
@@ -1,12 +1,36 @@
 //welcom to Rock Paper and Scissors  Game !!!
-console.log( "Welcom to play Rock Paper and Scissons ")
+console.log( "Welcom to play Rock Paper and Scissors ")
 
 console.log("choose one of the liste : Rock , Paper , Scissors ! Enjoy your Game");
-let playerMove =  Process.argv.slice(2); //input player choice 
-let choice = [ 'Rock' , 'Paper' , 'Scissors']; 
-let computerMove  =  choice( math.floor(Math.random() * 3 ));
+let playerMove = process.argv[2];
+const choice  = [ 'Rock' , 'Paper' , 'Scissors'];
+ function randomChoice () {
+    const random = Math.floor(Math.random () * choice.length); 
+    return choice [ random];
+ }
+ console.log
+ const computerMove = randomChoice () ;
+function result() {
+   console.log(`You chose ${playerMove}. Computer chose ${computerMove}. You win!`);
 
-if ( choice.includes(playerMove));   // test if the player choose one of the right list to play the game 
-console.log("please choose one of this liste : [ Rock, Paper, Scissors");
-Process.exit();
+ if (playerMove === computerMove) {
+    console.log("Draw! You can play again.");
+  } else if (
+    (computerMove === 'Scissors' && playerMove === 'Paper') ||
+    (computerMove === 'Paper' && playerMove === 'Rock') ||
+    (computerMove === 'Rock' && playerMove === 'Scissors')
+  ) {
+    console.log("You lose! Try again.");
+  } else if (
+    (playerMove === 'Scissors' && computerMove === 'Paper') ||
+    (playerMove === 'Paper' && computerMove === 'Rock') ||
+    (playerMove === 'Rock' && computerMove === 'Scissors')
+  ) {
+    console.log("You win!");
+  } else {
+    console.log("Invalid input. Please choose Rock, Paper, or Scissors.");
+  }
+  }
 
+
+result();
