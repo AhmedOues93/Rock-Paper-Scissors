96fd1b0afa723ccb0c3e3297a402e535ed4ca718
ahmed oueslati <oueslatiahmed38@gmail.com>
2025-07-06 11:13:50 +0200

    end

--- Rps.js
+++ Rps.js
@@ -1,19 +1,36 @@
-//welcom to Rock Paper and Scissors  Game !!!
-console.log( "Welcom to play Rock Paper and Scissors ")
+console.log("Welcome to play Rock Paper and Scissors!"); //Willkommensnachricht im Terminal
 
-console.log("choose one of the liste : Rock , Paper , Scissors ! Enjoy your Game");
 let playerMove = process.argv[2];
-const choice  = [ 'Rock' , 'Paper' , 'Scissors'];
- function randomChoice () {
-    const random = Math.floor(Math.random () * choice.length); 
-    return choice [ random];
- }
- console.log
- const computerMove = randomChoice () ;
-function result() {
-   console.log(`You chose ${playerMove}. Computer chose ${computerMove}. You win!`);
 
- if (playerMove === computerMove) {
+// Wenn der spieler nichts eigeben hat 
+if (!playerMove) {
+  console.log("Please choose Rock, Paper, or Scissors");
+  return;
+}
+
+console.log("Choose one of the list: Rock, Paper, Scissors! Enjoy your Game");
+
+const choice = ['Rock', 'Paper', 'Scissors'];
+
+// Funktion, die zufällig einen Spielzug für den Computer auswählt
+function randomChoice() {
+  const random = Math.floor(Math.random() * choice.length);
+  return choice[random];
+}
+
+const computerMove = randomChoice();
+
+//funktion wenn der Spieler etwas falsh angegebn hat 
+function result() {
+  if (!choice.includes(playerMove)) {
+    console.log("Invalid input. Please choose Rock, Paper, or Scissors.");
+    return;
+  }
+
+  console.log(`You chose ${playerMove}. Computer chose ${computerMove}`); // Zeigt die Auswahl beider Spieler an
+
+  //  mit if und else statment Vergleicht den Spielzug des Spielers mit dem des Computers und bestimmt, wer gewinnt
+  if (playerMove === computerMove) {
     console.log("Draw! You can play again.");
   } else if (
     (computerMove === 'Scissors' && playerMove === 'Paper') ||
@@ -21,16 +38,15 @@ function result() {
     (computerMove === 'Rock' && playerMove === 'Scissors')
   ) {
     console.log("You lose! Try again.");
-  } else if (
-    (playerMove === 'Scissors' && computerMove === 'Paper') ||
-    (playerMove === 'Paper' && computerMove === 'Rock') ||
-    (playerMove === 'Rock' && computerMove === 'Scissors')
-  ) {
-    console.log("You win!");
   } else {
-    console.log("Invalid input. Please choose Rock, Paper, or Scissors.");
+    console.log("You win!");
   }
-  }
-
-
+}
+// Funktion wird aufgerufen, um das Ergebnis zu berechnen
 result();
+
+   
+  
+
+
+
